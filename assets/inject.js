434372ca3b55838a0559a44a9fd190f752b8ca4f
Dylan Grant <dgrant069@hotmail.com>
2014-05-06 16:30:39 -0700

    not sure why js isn't loading

--- assets/inject.js
+++ assets/inject.js
@@ -1,5 +1,9 @@
+window.alert("INJECT LOADED");
+Toast.makeText(MainActivity.this, "INJECT LOADED", Toast.LENGTH_SHORT).show();
+
 function doSomething() {
     JSBridge.onDidSomething();
+    Toast.makeText(MainActivity.this, "INJECT SOMETHING", Toast.LENGTH_SHORT).show();
 }
 
 function launchCamera() {
@@ -7,6 +11,8 @@ function launchCamera() {
 }
 
 function injectTakePictureOnClick() {
+    Toast.makeText(MainActivity.this, "CAMERA INIT", Toast.LENGTH_SHORT).show();
     var takePicBtn = document.getElementsByClassName('step1')[0]; // This is the <a> element
-    takePicBtn.onclick = launchCamera;
+    takePicBtn.onclick = launchCamera; // should launchCamera have ()
+    Toast.makeText(MainActivity.this, "CAMERA LAUNCH", Toast.LENGTH_SHORT).show();
 }
--- src/com/ghostruck/android/MainActivity.java
+++ src/com/ghostruck/android/MainActivity.java
@@ -23,51 +23,52 @@ import android.widget.Toast;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
-    
+
     private static final String BASE_URL = "https://m.ghostruck.com";
     private static final int PICK_CAMERA_IMAGE = 2;
 
     private WebView mWebView;
     private ProgressBar mProgressBar;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         // Defaults to visible.  We will hide on onPageFinished
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        
+
         mWebView = (WebView) findViewById(R.id.web_view);
         mWebView.addJavascriptInterface(new JSBridge(), "JSBridge");
         mWebView.setWebChromeClient(new MyWebChromeClient());
         mWebView.setWebViewClient(new MyWebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
-        
+
         // Load url
         mWebView.loadUrl(BASE_URL);
         loadInjectJS();
     }
-    
+
     protected void loadInjectJS() {
         mWebView.loadUrl(getJs("inject.js"));
+        // mWebView.loadUrl("file:///android_asset/inject.js");
     }
-    
+
     protected void doSomething() {
         callJavascriptFunc("doSomething", "");
     }
-    
+
     protected void injectTakePictureOnClick() {
         callJavascriptFunc("injectTakePictureOnClick", "");
     }
-    
+
     protected void callJavascriptFunc(final String function, final String args) {
-        
+
         runOnUiThread(new Runnable() {
-            
+
             @Override
             public void run() {
-                // Wrapping in a try to catch any exceptions in case this tries to 
+                // Wrapping in a try to catch any exceptions in case this tries to
                 // run after the WebView has been destroyed
                 try {
                     mWebView.loadUrl(String.format("javascript:%s('%s');", function, args));
@@ -77,9 +78,9 @@ public class MainActivity extends Activity {
             }
         });
     }
-    
+
     private class MyWebChromeClient extends WebChromeClient {
-        
+
         // There are a bunch of methods you can override here, i.e.:
         @Override
         public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
@@ -88,36 +89,36 @@ public class MainActivity extends Activity {
             //  Do something with it
         }
     }
-    
+
     private class MyWebViewClient extends WebViewClient {
-        
+
         @Override
         public void onReceivedError(WebView view, int errorCode,
                 String description, String failingUrl) {
             super.onReceivedError(view, errorCode, description, failingUrl);
-            
+
             // Handle error
             Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
         }
-        
+
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            
+
             mProgressBar.setVisibility(View.GONE);
-            Toast.makeText(MainActivity.this, "Page finished, now we'll do something", Toast.LENGTH_SHORT).show();
             doSomething();
             injectTakePictureOnClick();
+            Toast.makeText(MainActivity.this, "READY TO TAKE PHOTO", Toast.LENGTH_SHORT).show();
         }
     }
-    
+
     protected final class JSBridge {
-        
+
         // Add javascript callbacks here - the javascript file is in assets/inject.js
         @JavascriptInterface
         public void onDidSomething() {
             runOnUiThread(new Runnable() {
-                
+
                 @Override
                 public void run() {
                     Toast.makeText(MainActivity.this, "We did something", Toast.LENGTH_SHORT).show();
@@ -128,7 +129,7 @@ public class MainActivity extends Activity {
         @JavascriptInterface
         public void onLaunchCamera() {
             runOnUiThread(new Runnable() {
-                
+
                 @Override
                 public void run() {
                     Toast.makeText(MainActivity.this, "Launching camera", Toast.LENGTH_SHORT).show();
@@ -137,7 +138,7 @@ public class MainActivity extends Activity {
             });
         }
     }
-    
+
     // Get our javascript files from assets
     protected String getJs(String fileName) {
         InputStream is;
@@ -150,10 +151,10 @@ public class MainActivity extends Activity {
             return null;
         }
     }
-    
+
     @Override
     public void onBackPressed() {
-        
+
         // Handle back in browser history
         if (!mWebView.getUrl().equals(mWebView.getOriginalUrl())) {
             mWebView.goBack();
@@ -162,18 +163,18 @@ public class MainActivity extends Activity {
             super.onBackPressed();
         }
     }
-    
+
     protected void launchCamera() {
         // see http://stackoverflow.com/questions/16799818/open-camera-using-intent
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, PICK_CAMERA_IMAGE);
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        
+
         switch (requestCode) {
             case PICK_CAMERA_IMAGE:
                 if (resultCode == RESULT_OK) {
@@ -181,7 +182,7 @@ public class MainActivity extends Activity {
                 }
                 break;
         }
-        
+
     }
 
 }
